8dd472e2d0b5d9d7fe6989d89700a5c7b45dd392
Rohit Yadav <rohity123456@gmail.com>
2024-03-28 17:02:01 +0530

    Add CORS middleware to the backend app

--- backend/app.ts
+++ backend/app.ts
@@ -7,11 +7,14 @@ import mongoose from "mongoose";
 import http from "http";
 import { initializeSocketIO } from "socket";
 import apiRouter from "@/routes/index";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
+
 const server = http.createServer(app);
 initializeSocketIO(server);
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
