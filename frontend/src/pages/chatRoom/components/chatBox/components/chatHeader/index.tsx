7708333217acf77845b4af14b9d0a9c16902ec41
Rohit Yadav <rohity123456@gmail.com>
2024-03-30 05:12:48 +0530

    Add online status in chat Header

--- frontend/src/pages/chatRoom/components/chatBox/components/chatHeader/index.tsx
+++ frontend/src/pages/chatRoom/components/chatBox/components/chatHeader/index.tsx
@@ -1,4 +1,4 @@
-import { Avatar } from "antd";
+import { ActiveStatus } from "@/global/constants";
 import styles from "./index.module.scss";
 import { IUser } from "@/types";
 
@@ -7,10 +7,11 @@ interface ChatHeaderProps {
 }
 const ChatHeader: React.FC<ChatHeaderProps> = ({ recipient }) => {
   if (!recipient) return null;
+  const isActive = recipient.status === ActiveStatus.ACTIVE;
   return (
     <div className={styles["chatHeader"]}>
       <div className={styles["avatar"]}>
-        <Avatar />
+        <div className={`${isActive ? "greenDot" : "redDot"}`}></div>
         <p>{recipient?.username}</p>
       </div>
     </div>
